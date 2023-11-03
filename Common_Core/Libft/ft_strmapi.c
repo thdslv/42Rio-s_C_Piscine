afdd1c0df75390c0872dac09f2ea959d92d4ce8c
thdslv <105087909+thdslv@users.noreply.github.com>
2023-11-03 22:49:51 +0000

    Libft

--- Common_Core/Libft/ft_strmapi.c
+++ Common_Core/Libft/ft_strmapi.c
@@ -11,10 +11,35 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+void	function(unsigned int x, char *p)
+{
+	
+}
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	char	*str;
+	size_t	i;
+	if (!s)
+		return (NULL);
+	str = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (*(s + i) != '\0')
+	{
+		*(str + i) = f(i, *(s + i));
+		i++;
+	}
+	*(str + i) = '\0';
+	return (str);
+}
 
+int	main()
+{
+	char	str[] = "test";
 
+	ft_strmapi(str, function);
+	return (0);
 }
