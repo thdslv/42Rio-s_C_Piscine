906f692bcb77200e9650d7fde26af0ffd2f56c8a
thda-sil <indiegamer3.0@gmail.com>
2023-11-07 21:28:40 -0300

    Libft

--- Common_Core/Libft/ft_split.c
+++ Common_Core/Libft/ft_split.c
@@ -6,13 +6,14 @@
 /*   By: thda-sil <thda-sil@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 18:20:54 by thda-sil          #+#    #+#             */
-/*   Updated: 2023/11/06 20:13:58 by thda-sil         ###   ########.fr       */
+/*   Updated: 2023/11/07 21:27:40 by thda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-size_t	w_count(char const *s, char c)
+size_t	word_count(char const *s, char c)
 {
 	size_t	words;
 	size_t	i;
@@ -21,48 +22,68 @@ size_t	w_count(char const *s, char c)
 	i = 0;
 	while (s[i])
 	{
-		if ((s[i] == c) && (s[i - 1] != c))
+		if ((s[i] == c) && (i > 0) && (s[i - 1] != c))
 			words++;
 		i++;
 	}
-	if ((s[i] == '\0') && (s[i - 1] != c))
-		words++;	
+	if ((s[i] == '\0') && (i > 0) && (s[i - 1] != c))
+		words++;
 	return (words);
+}
+
+static void	allocation(char **split, char const *s, char c, size_t w_count)
+{
+	size_t	i;
+	size_t	x;
+
+	x = 0;
+	while (x < w_count)
+	{
+		while (*s == c)
+			s++;
+		i = 0;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+		split[x] = (char *)malloc((i + 1) * sizeof(char));
+		if (!split[x])
+			break ;
+		if (i)
+			ft_strlcpy(split[x], s, i + 1);
+		s += i;
+		x++;
+	}
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**split;
-	size_t	i;
-	size_t	x;
+	size_t	w_count;
 
 	if (!s)
 		return (NULL);
-	split = (char **)ft_calloc((w_count(s, c) + 1), sizeof(char *));
+	w_count = word_count(s, c);
+	split = (char **)malloc((w_count + 1) * sizeof(char *));
 	if (!split)
 		return (NULL);
-	x = 0;
-	while (x < w_count)
-	{
-		i = 0;
-		while (s[i] != c && s[i] != '\0')
-		{
-			split[x][i] = s[i];
-			i++;
-		}
-		x++;
-	}
+	allocation(split, s, c, w_count);
+	split[w_count] = 0;
 	return (split);
 }
 
-#include <stdio.h>
+/*int	main()
+{
+	char	*str = "_UNDEUXTROIS";
+	char	c = '_';
+	size_t	i = 0;
+	size_t	word_c = word_count(str, c) + 1;
+	char	**split;
 
-int	main()
-{//incluir as palavras na split
-	char	str[] = "   teste teste    testeeeeeee";
-	char	c = ' ';
-
-	//printf("%s", ft_split(str));
-	printf("%zu", w_count(str, c));
+	split = ft_split(str, c);
+	while (word_c > 0)
+	{
+		printf("%s\n", split[i]);
+		i++;
+		word_c--;
+	}
 	return (0);
-}
+}*/
