fbc936f1e7b5aa20272e93bfe87383680b6519dd
thda-sil <indiegamer3.0@gmail.com>
2023-12-22 18:16:43 -0300

    get_next_line

--- Common_Core/get_next_line/get_next_line.c
+++ Common_Core/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: thda-sil <thda-sil@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 15:21:18 by thda-sil          #+#    #+#             */
-/*   Updated: 2023/12/02 16:05:41 by thda-sil         ###   ########.fr       */
+/*   Updated: 2023/12/22 18:14:34 by thda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,11 @@ char	*get_next_line(int fd)
 #include <stdio.h>
 int	main(void)
 {
+	int	fd;
+	char	*line;
+
+	fd = open(file.txt, O_RDONLY);
+	str = get_next_line(fd);
+	free();
 	return (0);
 }
