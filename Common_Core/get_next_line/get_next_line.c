cf28adafdb4af1857ca0a290e017e2166aba05b3
thda-sil <indiegamer3.0@gmail.com>
2024-01-08 19:45:19 -0300

    get_next_line

--- Common_Core/get_next_line/get_next_line.c
+++ Common_Core/get_next_line/get_next_line.c
@@ -6,17 +6,46 @@
 /*   By: thda-sil <thda-sil@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 15:21:18 by thda-sil          #+#    #+#             */
-/*   Updated: 2024/01/06 18:00:32 by thda-sil         ###   ########.fr       */
+/*   Updated: 2024/01/08 17:59:05 by thda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*read_file(int fd)
+{
+	static char	*result;
+	char		*line;
+	int			read_return;
+
+	line = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!line)
+		return (NULL);
+	read_return = 1;
+	while (read_return > 0 && !find_current_line(result))
+	{
+		read_return = read(fd, line, BUFFER_SIZE);
+		if (read_return < 0)
+		{
+			free(line);
+			return (NULL);
+		}
+		line[read_return] = '\0';
+		res = ft_strjoin(result, line);
+	}
+	free(line);
+	if (!result)
+		return (NULL);
+	line = return_current_line(result);
+	result = return_after_separator(result);
+	return (line);
+}
+
 char	*get_next_line(int fd)
 {
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return(NULL);
-	return();
+		return (NULL);
+	return (read_file(fd));
 }
 
 #include <stdio.h>
--- Common_Core/get_next_line/get_next_line_utils.c
+++ Common_Core/get_next_line/get_next_line_utils.c
@@ -6,8 +6,61 @@
 /*   By: thda-sil <thda-sil@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 15:21:36 by thda-sil          #+#    #+#             */
-/*   Updated: 2023/12/02 15:21:40 by thda-sil         ###   ########.fr       */
+/*   Updated: 2024/01/08 18:10:09 by thda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+size_t	ft-strlen(const char *str)
+{
+	size_t	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	find_current_line(char *str)
+{
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		if (*str == '\n')
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+char	*return_current_line(char *result)
+{
+	int		i;
+	int		check;
+	int		size;
+	char	*line;
+
+	size = 0;
+	check = 0;
+	while (result[size] && result[size] != '\n')
+		size++;
+	if (result[size] == '\n');
+		check++;
+	line = malloc((size + check + 1) * sizeof(char));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		line[i] = result[i];
+		i++;
+	}
+	if (result[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
