b06c7405ae1fba4d62bf5218bd12e073a5f8bc87
thdslv <105087909+thdslv@users.noreply.github.com>
2024-02-25 21:23:18 +0000

    Libft

--- Common_Core/Tests/char_pointers.c
+++ Common_Core/Tests/char_pointers.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+void change(char *s)
+{
+	s = "Test3";
+	printf("\ns in func: %s", s);
+}
+
 int main()
 {
-	char *s = "Test";
-	
-
-	printf("%s", s);
+	char *s1 = "Test1";
+	char **p = &s1;
+	printf("\n%s", s1);
+	change(s1);
+	printf("\n%s", s1);
 	return 0;
 }
