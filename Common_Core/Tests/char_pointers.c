a9dacab082c94c06f2099c1408577f5b399ab15a
thdslv <105087909+thdslv@users.noreply.github.com>
2024-02-26 21:17:45 +0000

    Libft

--- Common_Core/Tests/char_pointers.c
+++ Common_Core/Tests/char_pointers.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-void change(char *s)
-{
-	s = "Test3";
-	printf("\ns in func: %s", s);
+void change(char **str)
+{	// str is a char** so it has the & to a char*
+	*str = "Test2"; // str is &s1 so derreferencing it (*str)
+			// gives us s1 ("Test") s1 has &'t' so *s = 
+	printf("\ns in func: %s", *str);
 }
 
 int main()
 {
-	char *s1 = "Test1";
-	char **p = &s1;
+	char *s1 = "Test1"; // s1 = the adress of 'T'
+	char **p = &s1; // p = the adress of s1
 	printf("\n%s", s1);
-	change(s1);
+	change(&s1); // the adress of s1 (&s || p) is sent
 	printf("\n%s", s1);
 	return 0;
 }
